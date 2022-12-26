949ed2bacc0133a1ed2e463e2214cea00fbd0526
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-12-26 15:32:15 -0800

    Created instance variables

--- tech-news-java-api/tech-news-java-api/src/main/java/com/technews/model/User.java
+++ tech-news-java-api/tech-news-java-api/src/main/java/com/technews/model/User.java
@@ -8,4 +8,16 @@ import jakarta.persistence.Entity;
 @Table(name = "user")
 
 public class User {
+
+        private Integer id;
+        private String username;
+        private String email;
+        private String password;
+        boolean loggedIn;
+
+        private List<Post> posts;
+        private List<Vote> votes;
+        private List<Comment> comments;
+
+
 }
