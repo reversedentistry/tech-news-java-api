350bd4d2adf903d6a48e7d286aad27917ebe3ae4
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-12-26 14:09:41 -0800

    Added more practice

--- tech-news-java-api/tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
+++ tech-news-java-api/tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -2,6 +2,12 @@ package com.technews;
 
 public class VariablesPractice {
     public static void main(String[] args) {
+        int demoInt = 5;
+        double demoDouble = 5.5;
+        System.out.println(demoInt + demoDouble); //result should be a double
 
+        String demoString = "Demo";
+
+        System.out.println(demoInt + demoString); 
     }
 }
