fa7212a9f098fd1d6daa0ea2415f4ea8acd06834
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-12-26 15:35:43 -0800

    Added instance level annotations

--- tech-news-java-api/tech-news-java-api/src/main/java/com/technews/model/User.java
+++ tech-news-java-api/tech-news-java-api/src/main/java/com/technews/model/User.java
@@ -1,23 +1,29 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Transient
+    boolean loggedIn;
 
-        private Integer id;
-        private String username;
-        private String email;
-        private String password;
-        boolean loggedIn;
-
-        private List<Post> posts;
-        private List<Vote> votes;
-        private List<Comment> comments;
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Post> posts;
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Vote> votes;
+    @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> comments;
 
 
 }
