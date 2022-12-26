2579c3f42bef8a3522c323cd2b232943259b8783
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-12-26 14:21:50 -0800

    Created constructors and getter + setters

--- tech-news-java-api/tech-news-java-api/src/main/java/com/technews/testModel/demo.java
+++ tech-news-java-api/tech-news-java-api/src/main/java/com/technews/testModel/demo.java
@@ -1,5 +1,26 @@
 package com.technews.testModel;
 
 public class demo {
+    private String name;
+    private int age;
+    public demo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
