3b669a9f6ddfb2dd06ec504947915372831ce197
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-12-26 16:21:11 -0800

    Added annotations

--- tech-news-java-api/tech-news-java-api/src/main/java/com/technews/model/Post.java
+++ tech-news-java-api/tech-news-java-api/src/main/java/com/technews/model/Post.java
@@ -1,13 +1,35 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
+public class Post implements Serializable {
+
+}
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String title;
     private String postUrl;
+    @Transient
     private String userName;
+    @Transient
     private int voteCount;
     private Integer userId;
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "posted_at")
     private Date postedAt = new Date();
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "posted_at")
     private Date updatedAt = new Date();
     private List<Comment> comments;
 }
