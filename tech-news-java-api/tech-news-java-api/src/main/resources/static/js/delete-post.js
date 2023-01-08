07c6eed94d19746cb3c9f9b33d58c4d03554780e
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2023-01-08 15:52:31 -0800

    Created JS post deletion functionality

---
+++ tech-news-java-api/tech-news-java-api/src/main/resources/static/js/delete-post.js
@@ -0,0 +1,20 @@
+async function deleteFormHandler(event) {
+  event.preventDefault();
+
+//Capture ID of post
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/')
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
